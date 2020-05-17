1a9455890f6c04871e97655711e825230aa57b50
Nicholas Fischer <fischern4293@uwec.edu>
2020-05-16 20:38:27 -0500

    Build try for repo

---
+++ precache-manifest.5645e0de3e98733c9625ca2287b4e984.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa7e3c3fbe425b80bcda34254f62483",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6f4c81d85e949477021",
+    "url": "/static/css/2.53823a3d.chunk.css"
+  },
+  {
+    "revision": "f2a4c955f0ac08be60ad",
+    "url": "/static/css/main.907c89b7.chunk.css"
+  },
+  {
+    "revision": "b6f4c81d85e949477021",
+    "url": "/static/js/2.f24351f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f24351f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2a4c955f0ac08be60ad",
+    "url": "/static/js/main.8f853ec4.chunk.js"
+  },
+  {
+    "revision": "547087cc408e3b1a499f",
+    "url": "/static/js/runtime-main.cecad5d0.js"
+  }
+]);
--- src/http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "165.227.123.95:3443",
-  headers: {
-    "Content-type": "application/json"
-  }
-});
